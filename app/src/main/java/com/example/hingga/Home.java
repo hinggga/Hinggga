d6fc07a6e5d3b33628b9ae57fa0f298e661202c6
Hingga Kriskurniawan <54565407+hinggga@users.noreply.github.com>
2020-03-05 22:29:19 +0700

    latihan4

--- app/src/main/java/com/example/hingga/Home.java
+++ app/src/main/java/com/example/hingga/Home.java
@@ -1,14 +1,68 @@
 package com.example.hingga;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class Home extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem tab1,tab2,tab3;
+    public PageAdapter pagerAdapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tab1 = (TabItem) findViewById(R.id.Tab1);
+        tab2 = (TabItem) findViewById(R.id.Tab2);
+        tab3 = (TabItem) findViewById(R.id.Tab3);
+        viewPager = findViewById(R.id.viewpager);
+
+        pagerAdapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 1 ){
+                    pagerAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 2 ){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
     }
 }
--- app/src/main/java/com/example/hingga/MainActivity.java
+++ app/src/main/java/com/example/hingga/MainActivity.java
@@ -6,14 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
 
 
         TextView daftar = (TextView)findViewById(R.id.txtDaftar);
